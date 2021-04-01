7aaed6dafb4b8ae57fe63ab88d65ff871220a4ba
Jordy Tshibangu <jordytshibss@gmail.com>
2021-04-01 15:09:47 +0200

    features : build schema and resolver

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import { graphqlHTTP, } from 'express-graphql';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+import schema from './schema'; 
+
 
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
 }));
 
-app.listen(PORT, () => {
-    console.log(`The server is running on port ${PORT}`);
-})
+app.listen(PORT, () =>  console.log(`The server is running on port ${PORT}`));
