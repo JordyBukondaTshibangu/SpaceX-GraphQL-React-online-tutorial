523b3b631e568ec16e0d8b235419abeaf7325b34
Jordy Tshibangu <jordytshibss@gmail.com>
2021-04-10 02:27:02 +0200

    fix : models import into resolvers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { graphqlHTTP, } from 'express-graphql';
-import schema from './schema/index.js'; 
+import schema from './grapqhl/schema/index.js'
 import './database/db.js';
 import cors from 'cors';
 
